0cbf2ae93c782955ee40d225c21c00d861ea3317
Georgi Nedyalkov <gnedyalkov94@gmail.com>
2025-05-12 18:10:26 +0300

    feat: added trouble and lualine plugins

--- lua/g/lazy/lsp.lua
+++ lua/g/lazy/lsp.lua
@@ -43,7 +43,6 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "rust_analyzer",
 				"ts_ls",
             },
             handlers = {
